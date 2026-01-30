64d459122e02fd73c702cfff90f00ac16665eb60
luckydonald <m1-mac-2024._.code@luckydonald.de>
2026-01-30 14:58:54 +0100

    📄TEMPLATE | 📦👩‍💻 Allow to easily add more optional stuff in the future.

--- frontend_vue/eslint.config.js
+++ frontend_vue/eslint.config.js
@@ -1,16 +1,25 @@
 import base from './eslint.base.js';
 import ts from './eslint.ts.js';
 
-let vue = [];
-try {
-  // Only import if vue is installed
-  vue = await import('./eslint.vue.js').then(m => m.default ?? []);
-} catch {
-  // vue not installed, silently skip
-}
+/** @type { string[] } */
+const OPTIONAL_CONFIGS = [
+  'vue',
+];
+
+const optionals = await Promise.allSettled(
+  OPTIONAL_CONFIGS.map(
+    async (configName) => {
+      try {
+        return await import((`./eslint.${configName}.js`)).then(m => m.default ?? []);
+      } catch {
+        return [];
+      }
+    }
+  )
+);
 
 export default [
   ...base,
   ...ts,
-  ...vue,
+  ...optionals,
 ];
