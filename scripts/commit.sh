8f9261f276df55147e01169dfedaa340a6c980a5
luckydonald <m1-mac-2024._.code@luckydonald.de>
2026-02-02 00:07:09 +0100

    📄TEMPLATE | ⏰¹ | 👩‍💻 Fix fronend lock file failing to commit.
    ¹) taken from https://github.com/luckydonald/hoass_calendar-alarm-clock/commit/161c6de40a8e10a43c773b779c792ca427518a96)

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -99,7 +99,7 @@ if git diff --name-only | grep -qE '^(frontend|frontend_vue)/(yarn.lock|package-
       frontend/package-lock.json \
       frontend_vue/yarn.lock \
       frontend_vue/package-lock.json \
-      2>/dev/null || true
+      --ignore-missing
     # Commit with templating
     git commit -m "${COMMIT_PREFIX}$(lock_type="frontend" tmpl "${COMMIT_MSG_LOCK}")"
     echo "  Done"
